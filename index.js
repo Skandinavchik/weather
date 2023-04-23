fd77bdf9bdc96542b364d2184401525ceed39ceb
Artem Sokolov <artemsokolov@lostriver.eu>
2023-04-23 10:49:04 +0200

    storage.service added

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import { getArgs } from "./helpers/args.js";
 import { printError, printSuccess, printHelp } from './services/log.service.js';
+import { saveKeyValue } from "./services/storage.service.js";
 
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Token saved');
+    } catch (error) {
+        printError(error.message);
+    }
+};
 
 const initCli = () => {
     const args = getArgs(process.argv);
 
-    console.log(args);
     if (args.h) {
         // show help
         printHelp();
@@ -13,11 +22,11 @@ const initCli = () => {
 
     if (args.s) {
         // save city
-        printSuccess();
     }
-
+    
     if (args.t) {
         // save token
+        return saveToken(args.t);
     }
 
     // show weather
